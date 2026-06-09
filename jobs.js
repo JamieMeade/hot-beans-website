fd86c31edd7a73b94428c7d9ccd055256befac4b
JamieMeade <5294657@student.gloscol.ac.uk>
2026-06-09 15:12:06 +0100

    Delete jobs.js

--- jobs.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll(".job-card");
-
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    cards.forEach(c => c.classList.remove("active"));
-    card.classList.add("active");
-  });
-});
